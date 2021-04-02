d22ac0b23580f4636ceef782f041a7cc9d29e0ed
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2021-04-02 17:59:38 +0100

    adding error handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {ApolloServer, gql} = require('apollo-server');
+const {ApolloServer, ApolloError} = require('apollo-server');
 const TeaAPI = require('./dataSources/dataSets')
 const ProducersAPI = require('./dataSources/dataSets')
 
@@ -10,7 +10,18 @@ const dataSources = ()=>({
     TeaAPI: new TeaAPI(),
     ProducersAPI: new ProducersAPI()
 })
-const server = new ApolloServer({typeDefs, resolvers, dataSources});
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers, 
+    dataSources, 
+    debug: false,
+    formatError: (err) => {
+        if(err.extensions.code == "INTERNAL_SERVER_ERROR"){
+            return new ApolloError("This is nicer message than to show you 500", "ERROR")
+        }
+        return err
+    }
+});
 
 server
 .listen({port: process.env.PORT || 4000})
--- resolvers/tea.js
+++ resolvers/tea.js
@@ -1,9 +1,16 @@
+const { ApolloError } = require("apollo-server");
+
 module.exports = {
     producer: (tea, args, {dataSources}) => {
+    try{
         const producers = dataSources.ProducersAPI.getProducers()
         const returned = producers.find((producer) => {
             return tea.producer.id === producer.id
         })
         return returned;
+    }catch( error ){
+        new ApolloError("cannot get producer")
+    }
+        
     }
 }
