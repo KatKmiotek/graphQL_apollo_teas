876e8d5a175d60d4a55cda32d1c946c0a0921001
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2021-04-25 19:04:11 +0100

    building

--- functions/teas/teas.js
+++ functions/teas/teas.js
@@ -18,14 +18,7 @@ const httpServer = createServer(app)
 
 mongoose
 .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.s5jz7.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true })
-// .then( (res) => {
-//     httpServer.listen(8888, () => {
-//         // console.log('connected! on server http://localhost:3000/graphql')
-//     })
-// })
-// .catch( (err) => {
-//     console.error('Error while connecting to MongoDB', err);
-// })
+
 
 const handler = server.createHandler()
 
